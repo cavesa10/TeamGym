be0ed2851982ddf7a040453e5ecb730191a50363
Carlos Vesga <cavesa10@gmail.com>
2021-10-14 18:16:32 -0500

    TM-62 Despliegue de la app a  Heroku

--- TeamGym/settings.py
+++ TeamGym/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import django_heroku
+
+
+django_heroku.settings(locals()) # configuracion de heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
