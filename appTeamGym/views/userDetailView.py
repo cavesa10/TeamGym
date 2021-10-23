4f987ddd135552a17964b15590070af1c1edf602
Carlos Vesga <cavesa10@gmail.com>
2021-10-23 17:41:30 -0500

    Update patch

--- appTeamGym/views/userDetailView.py
+++ appTeamGym/views/userDetailView.py
@@ -21,3 +21,13 @@ class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
             return Response(stringResponse,
                             status=status.HTTP_401_UNAUTHORIZED)
         return super().get(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data = tokenBackend.decode(token, verify=False)
+        if valid_data['user_id'] != kwargs['pk']: # compara id solicitante y a consultar
+            stringResponse = {'detail': 'Unauthorized Request'}
+            return Response(stringResponse,
+                            status=status.HTTP_401_UNAUTHORIZED)
+        return super().get(request, *args, **kwargs)
